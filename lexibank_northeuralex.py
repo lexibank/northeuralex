f8204861ec8a6d8770d7afb7ef4da3fa82b0a41b
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-10-29 10:38:54 +0100

    First changes as suggested by @Lingulist

--- lexibank_northeuralex.py
+++ lexibank_northeuralex.py
@@ -1,6 +1,6 @@
 import attr
+from pathlib import Path
 from clldutils.misc import slug
-from clldutils.path import Path
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank import Lexeme, Language
 from pylexibank.util import pb
@@ -23,7 +23,7 @@ class Dataset(BaseDataset):
     language_class = NLLanguage
 
     def cmd_download(self, args):
-        self.raw.download(
+        self.raw_dir.download(
             "http://www.northeuralex.org/static/downloads/northeuralex-cldf.csv",
             "nelex.tsv",
             log=self.log,
@@ -48,16 +48,17 @@ class Dataset(BaseDataset):
         args.writer.add_languages()
 
         # add the concepts from the concept list
-        args.writer.add_concepts(
-            id_factory=lambda c: concept_id(c.attributes["nelex_id"])
-        )
+        concept_lookup = args.writer.add_concepts(
+            id_factory=lambda c: "%s_%s" % (c.id.split("-")[-1],
+                slug(c.english)),
+            lookup_factory="Name")
 
         # add items
         lexeme_rows = self.raw_dir.read_csv("nelex.tsv", delimiter="\t", dicts=True)
         for row in pb(lexeme_rows, desc=f"Build CLDF for {self.id}"):
             lex = args.writer.add_form(
                 Language_ID=row["Language_ID"],
-                Parameter_ID=concept_id(row["Concept_ID"]),
+                Parameter_ID=concept_lookup[row["Concept_ID"]],
                 Value=row["rawIPA"],
                 Form=row["rawIPA"],
                 Orthography=row["Word_Form"],
