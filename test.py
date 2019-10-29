487db9a9c3d52f8dd54ea31c0ceebe738d70eed2
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-10-29 13:49:59 +0100

    Fixed tests

--- test.py
+++ test.py
@@ -2,13 +2,12 @@ def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
 
 def test_forms(cldf_dataset):
-    assert 1 == 1
+    assert len(list(cldf_dataset["FormTable"])) == 121612
+    assert any(f["Form"] == "silmæ" for f in cldf_dataset["FormTable"])
 
 def test_parameters(cldf_dataset):
-    assert 1 == 1
+    assert len(list(cldf_dataset["ParameterTable"])) == 1016
 
 def test_languages(cldf_dataset):
-    assert 1 == 1
+    assert len(list(cldf_dataset["LanguageTable"])) == 107
 
-def test_cognates(cldf_dataset):
-    assert 1 == 1
